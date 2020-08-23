9f479c08092e30ad54fa7fc15f4b1f3a7ec47d5c
[Gautam] <[mailforvijay3@gmail.com]>
2020-08-23 07:41:46 +0530

    first commit

---
+++ build/precache-manifest.e2f97dca9482ab773a495476010e274c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0321a582979a31c669c98a8c285c590",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39f20c1b4b855389ebe5",
+    "url": "/static/css/main.20e85d9f.chunk.css"
+  },
+  {
+    "revision": "3f12543f92572a7dcb7d",
+    "url": "/static/js/2.6f0201df.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.6f0201df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39f20c1b4b855389ebe5",
+    "url": "/static/js/main.17db45b2.chunk.js"
+  },
+  {
+    "revision": "3cf8ae71a09948a08a2e",
+    "url": "/static/js/runtime-main.4857cacc.js"
+  },
+  {
+    "revision": "60ba0f9261f7c67c07f10ddcce02b974",
+    "url": "/static/media/chat-logo-design_93835-108.60ba0f92.jpg"
+  }
+]);
